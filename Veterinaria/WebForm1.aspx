219593a052ef9feba56d64fdd21f9c050bce81d2
ERIC <ERIC@DESKTOP-RMLTTME>
2023-08-31 18:11:03 -0300

    Agregar archivos de proyecto.

---
+++ Veterinaria/WebForm1.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Veterinaria.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <br />
+            <asp:Label ID="Label3" runat="server" Text="Cliente: "></asp:Label>
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <br />
+            <br />
+
+            <asp:Label ID="Label1" runat="server" Text="Nombre Mascota: "></asp:Label>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <br />
+            <br />
+
+            <asp:Label ID="Label2" runat="server" Text="Especie: "></asp:Label>
+            <asp:RadioButtonList ID="RadioButtonList1" runat="server" OnSelectedIndexChanged="selectedRadioButton" AutoPostBack="True">
+                <asp:ListItem>Perro</asp:ListItem>
+            <asp:ListItem>Gato</asp:ListItem>
+            </asp:RadioButtonList>
+            <br />
+            
+            <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True"></asp:ListBox>
+            <br />
+
+            <asp:Button ID="Button1" runat="server" Text="Agregar" OnClick="Button1_Click" />
+            <br />
+            <br />
+
+            <asp:Label ID="Label4" runat="server" Text="Datos"></asp:Label>
+
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Veterinaria/WebForm1.aspx.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Veterinaria
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (TextBox1.Text !="" && TextBox2.Text !="" && RadioButtonList1.SelectedIndex >=0)
+            {
+                Label4.Text = $"Cliente: {TextBox1.Text}," + $"Mascota: {TextBox2.Text}," +
+                    $"Especie: {RadioButtonList1.SelectedItem.Text}," + $"Raza: {ListBox1.SelectedItem.Text},"  ;
+
+            }
+        }
+
+        protected void selectedRadioButton(object sender, EventArgs e)
+        {
+            String seleccion = RadioButtonList1.SelectedItem.Text; //rescatamos el valor de el radio seleccionado
+            ListBox1.Items.Clear(); //borre toda la lista si tiene losta cargada
+
+            if (seleccion == "Perro")
+            {
+                ListBox1.Items.Add(new ListItem ("Pitbull" , "Pit"));
+                ListBox1.Items.Add(new ListItem("Aleman", "Ale"));
+                ListBox1.Items.Add(new ListItem("Dogo", "Dog"));
+            }
+            else
+            {
+                ListBox1.Items.Add(new ListItem("Persa", "Per"));
+                ListBox1.Items.Add(new ListItem("Siames", "Sia"));
+                ListBox1.Items.Add(new ListItem("Exotico", "Exo"));
+            }
+        }
+    }
+}
